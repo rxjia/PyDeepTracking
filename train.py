347ce7a7cfc229e7613cc863b5c07fba84d19edb
rs <ruixingjia@outlook.com>
2020-05-22 15:45:18 +0800

    add readme

--- train.py
+++ train.py
@@ -14,14 +14,9 @@ data_file = os.path.join(data_dir, 'data.t1')
 
 from SensorData import LoadSensorData
 
-# DEFAULT_TENSOR_TYPE = torch.FloatTensor
-DEFAULT_TENSOR_TYPE = torch.cuda.FloatTensor
-torch.set_default_tensor_type(DEFAULT_TENSOR_TYPE)
-
-
-class NC:
-    pass
-
+def mkdir(dir):
+    if not os.path.exists(dir):
+        os.mkdir(dir)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='manual to this script')
@@ -31,7 +26,7 @@ if __name__ == '__main__':
     parser.add_argument('-model', type=str, default='model', help='neural network model')
     parser.add_argument('-data', type=str, default=data_file, help='training data')
     parser.add_argument('-learningRate', type=float, default=0.01, help='learning rate')
-    parser.add_argument('-initweights', type=float, default=0, help='initial weights')
+    parser.add_argument('-initweights', type=str, default=0, help='initial weights')
 
     parser.add_argument('-grid_minX', type=float, default=-25, help='occupancy grid bounds [m]')
     parser.add_argument('-grid_maxX', type=float, default=25, help='occupancy grid bounds [m]')
@@ -42,22 +37,16 @@ if __name__ == '__main__':
     parser.add_argument('-sensor_step', type=float, default=0.5, help='resolution of depth measurements [degrees]')
     params = parser.parse_args()
 
-    params = NC()
-    params.gpu = 0
-    params.iter = 100000
-    params.N = 100
-    params.model = 'model'
-    params.data = data_file
-    params.learningRate = 0.01
-    params.initweights = 0
-    params.grid_minX = -25
-    params.grid_maxX = 25
-    params.grid_minY = -45
-    params.grid_maxY = 5
-    params.grid_step = 1
-    params.sensor_start = -180
-    params.sensor_step = 0.5
+    # -- switch to GPU
+    if params.gpu > 0:
+        print('Using GPU :{}'.format(params.gpu))
+        DEFAULT_TENSOR_TYPE = torch.cuda.FloatTensor
+    else:
+        print('Using CPU')
+        DEFAULT_TENSOR_TYPE = torch.FloatTensor
+    torch.set_default_tensor_type(DEFAULT_TENSOR_TYPE)
 
+    # -- load training data
     data = LoadSensorData(params.data, params)
     width = data.width  # occupancy 2D grid width
     height = data.height  # occupancy 2D grid height
@@ -68,9 +57,10 @@ if __name__ == '__main__':
     # one step of RNN
     step = StepModule()
     # network weights + gradients
-    # w = step.parameters()
-    # print('Model has ' + str(len(list(w))) + ' parameters')
-    #
+    w = step.parameters()
+    print('Model has ' + str(len(list(w))) + ' parameters')
+
+    # TODO: add initweights
     # if params.initweights > 0:
     #     print('Loading weights ' + params.initweights)
     #     w.copy(torch.load(params.initweights))
@@ -78,13 +68,13 @@ if __name__ == '__main__':
     # chain N steps into a recurrent neural network
     model = Recurrent(step, params.N, width, height)
     print(model)
+
     # initial hidden state
     h0 = model.get_initial_state()
 
     # cost function
     # {y1, y2, ..., yN},{t1, t2, ..., tN} -> cost
     criterion = CustomLoss()
-
 
     # return i-th training sequence
     def getSequence(i):
@@ -100,6 +90,16 @@ if __name__ == '__main__':
     save_tensor_img(input[1][1], '/home/do/data/c.png')
     save_tensor_img(input[1][1] / 2 + input[1][0], '/home/do/data/d.png')
 
+    # create directory to save weights and videos
+    dir_weights = os.path.join(data_dir, 'weights_' + params.model)
+    dir_video = os.path.join(data_dir, 'video_' + params.model)
+    dir_video_input = os.path.join(dir_video, 'input')
+    dir_video_output = os.path.join(dir_video, 'output')
+
+    mkdir(dir_weights)
+    mkdir(dir_video)
+    mkdir(dir_video_input)
+    mkdir(dir_video_output)
 
     # filter and save model performance on a sample sequence
     def evalModel():
@@ -111,11 +111,10 @@ if __name__ == '__main__':
             torch.set_default_tensor_type(torch.FloatTensor)
             for i in range(len(input)):
                 save_tensor_img(input[i][1] / 2 + input[i][0],
-                                data_dir + '/video_' + params.model + '/ainput/' + str(i) + '.png')
+                                os.path.join(dir_video_input, '{}.png'.format(i)))
                 save_tensor_img(input[i][1] / 2 + output[i],
-                                data_dir + '/video_' + params.model + '/aoutput/' + str(i) + '.png')
+                                os.path.join(dir_video_output, '{}.png'.format(i)))
             torch.set_default_tensor_type(DEFAULT_TENSOR_TYPE)
-
 
     # blanks part of the sequence for predictive training
     def dropoutInput(target):
@@ -126,15 +125,6 @@ if __name__ == '__main__':
                 input[i].zero_()
         return input
 
-
-    # create directory to save weights and videos
-    dir = os.path.join(data_dir, 'weights_' + params.model)
-    if not os.path.exists(dir):
-        os.mkdir(dir)
-
-    dir = os.path.join(data_dir, 'video_' + params.model)
-    if not os.path.exists(dir):
-        os.mkdir(dir)
 
     optimizer = optim.Adagrad(model.parameters(), lr=params.learningRate)
     running_loss = 0.0
@@ -162,18 +152,16 @@ if __name__ == '__main__':
 
         # print statistics
         running_loss += loss.item()
-        if (k + 1) % p_size == 0:  # print every 2000 mini-batches
+        if (k + 1) % p_size == 0:  # print infos every p_size mini-batches
             print('Iteration' + str(k) + ', lost: ' + str(running_loss / p_size))
             running_loss = 0.0
 
-            if (k + 1) % s_size == 0:
-                for i in range(len(input)):
-                    save_tensor_img(output[i],
-                                    data_dir + '/video_' + params.model + '/output/' + str(i) + '.png')
+            if (k + 1) % s_size == 0:  # save infos every s_size
                 # save weights
                 state = {'net': model.state_dict(),
                          'optimizer': optimizer.state_dict(),
                          'k': k}
-                torch.save(state, data_dir + '/' + 'weights_' + params.model + '/' + str(k) + '.dat')
+                torch.save(state, os.path.join(dir_weights, '{}.dat'.format(k)))
+
                 # visualise performance
                 evalModel()
